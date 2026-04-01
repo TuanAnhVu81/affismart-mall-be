eb7ae60de15b6e9be3750e1e166ae3edce678698
TuanAnhVu81 <tuananhvu1123@gmail.com>
2026-04-01 14:59:05 +0700

    chore: initialize Spring Boot backend project

---
+++ src/main/java/com/affismart/mall/AffismartMallBeApplication.java
@@ -0,0 +1,13 @@
+package com.affismart.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffismartMallBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffismartMallBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/affismart/mall/AffismartMallBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affismart.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffismartMallBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
